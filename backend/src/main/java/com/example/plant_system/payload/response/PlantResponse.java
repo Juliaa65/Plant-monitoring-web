d872fd0c2846c6588c85a115fc3e9583e763af3b
Juliaa65 <ia.01.perevozniuk.yulia@gmail.com>
2024-06-10 18:43:57 +0300

    add payload

---
+++ backend/src/main/java/com/example/plant_system/payload/response/PlantResponse.java
@@ -0,0 +1,35 @@
+package com.example.plant_system.payload.response;
+
+import com.example.plant_system.dto.SensorDataDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class PlantResponse {
+    private Long id;
+
+    @NotBlank
+    private String plantName;
+
+    @NotBlank
+    private String type;
+
+    @NotBlank
+    private String info;
+
+    private String location;
+
+    private String age;
+
+    private String system_url;
+
+    private String camera_url;
+
+    private Long ownerId;
+
+    private SensorDataDto sensorData;
+}
---
+++ backend/src/main/java/com/example/plant_system/payload/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.example.plant_system.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class TokenResponse {
+
+    private String token;
+
+}
